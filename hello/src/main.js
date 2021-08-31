c737d303f9fea2a1be13f6d046b6a45c90d4471e
zhuzheng <linux0824@gmail.com>
2021-08-31 18:16:47 +0800

    侧边菜单

--- hello/src/main.js
+++ hello/src/main.js
@@ -1,6 +1,23 @@
+// 导入vue
 import { createApp } from 'vue'
+
+// 导入ElementPlus 组件
 import ElementPlus from 'element-plus'
+
+// 导入ElementPlus 样式
 import 'element-plus/dist/index.css'
+
+// 导入ElementPlus 中文组件
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+
+// 导入主UI组件
 import App from './App.vue'
 
-createApp(App).use(ElementPlus).mount('#app')
+// 创建app
+const app = createApp( App );
+
+// 使用ElementPlus
+app.use( ElementPlus, { locale: zhCn } );
+
+// 挂载到app节点
+app.mount( '#app' );
